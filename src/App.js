3bec3513eaa0ee256b6bd93ad2cea359a7801165
Yashaswi Anand <yash.kwr17@gmail.com>
2024-05-26 13:37:36 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Demo Project
+          Demo Project Yash
         </p>
         <a
           className="App-link"
